5cdaed4fe68e6dc88f4161710f5ea2bb29c0146f
kouroumadev <kouroumadev@gmail.com>
2024-04-04 12:15:23 +0000

    Update fiche-paie.blade.php

--- resources/views/files/decompte/fiche-paie.blade.php
+++ resources/views/files/decompte/fiche-paie.blade.php
@@ -4,62 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Carte Retraite</title>
+    <title>Fiche Paie</title>
 </head>
 <body style="font-family: 'Poppins',sans-serif">
     <div style="width: 100%;margin-bottom:10px;  margin:auto">
-
-
-    <table style="width:100%; position:absolute; top:-40; left:20">
-        <tbody>
-           <tr style="margin-top: 0 !important">
-
-               <td>
-                <img src="{{ public_path('LOgo-CNSS.png') }}" width="200" height="100">
-
-                </td>
-               <td>
-                  <span style="position: relative; top:20; font-size:15px;"> REPUBLIQUE DE GUINEE</span>
-
-                   <div style="width:75%; margin-left:0px;position: relative; top:20">
-                     <table style="width: 80%; position: relative; left:5">
-                        <tr>
-                            <td style="background-color: red; width:55%; height:2px"></td>
-                            <td style="background-color: yellow; width:55%"></td>
-                            <td style="background-color: green; width:55%"></td>
-                        </tr>
-                     </table>
-                  </div>
-                    <div style="margin-left: 20px;position: relative; top: 20 "> <span style="font-size:11px;">Travail-Justice-Solidarite</span></div><br><br>
-
-               </td>
-           </tr>
-           <tr>
-                <h3 style="text-align: center; position: relative; left:80">PENSION DE RETRAITE</h3>
-                <h1 style="text-align: center; position: relative; left:80">FICHE DE PAIE</h1>
-                <h2 style="text-align: center; position: relative; left:80"> <span style="color: red"> N° {{ $no_dossier }} </span> </h2>
-                <h2 style="text-align: center; position: relative; left:80">ASSIGNATION</h2>
-
-                <ol style="position: relative; left:150">
-                    <li>BOKÉ</li>
-                </ol>
-
-            </tr>
-        </tbody>
-    </table>
-
-
-
-
-         <table style="width: 100%; position:absolute; top:250">
+        <table style="width: 100%;margin-bottom:10px">
             <tbody>
                <tr style="margin-top: 0 !important">
 
-                  <td>
-                    <img src="{{ storage_path('app/public/pensionnaireImg/'.$photo) }}" style="width:100px; height:100px; margin-left:300px" alt="" srcset="">
+                   <td>
+                    <img src="{{ public_path('LOgo-CNSS.png') }}" width="200" height="100">
+
+                    </td>
+                   <td>
+                      <span style="position: relative; top:10; left:-15"> REPUBLIQUE DE GUINEE</span>
+
+
+                        <div style="margin-left: 10px;position: relative; top:15; font-size:12px"> Travail-Justice-Solidarite </div><br>
+
+                   </td>
+                   <td>
+                    {!! DNS2D::getBarcodeHTML($no_imma, 'QRCODE',3,3) !!}
+
                     </td>
 
                </tr>
+            </tbody>
+        </table>
+        <h3 style="text-align: center; ">PENSION DE RETRAITE</h3>
+        <h1 style="text-align: center; ">FICHE DE PAIE</h1>
+        <h2 style="text-align: center; "> <span style="color: red"> N° </span> </h2>
+        <h2 style="text-align: center; ">ASSIGNATION</h2>
+        <h4 style="text-align: center; ">BOKÉ</h4>
+
+
+
+
+        <div style="width: 100%;  position:absolute; top:220; left:20">
+
+
+            <span>
+                <img src="{{ storage_path('app/public/pensionnaireImg/'.$photo) }}" style="width:100px; height:100px;" alt="" srcset="">
+
+            </span>
+
+        </div>
+         <table style="width: 100%; margin-top:10px">
+            <tbody>
+
                <tr>
                 <table style="margin-top: 10px">
                     <tbody>
@@ -105,21 +97,26 @@
 
                 </table>
                </tr>
-               <tr>
-                <h3 style="text-align: center; position: relative; left:10">ENFANTS A CHARGE</h3>
-                <table style="width: 100%">
-                    <thead>
-                        <tr>
-                            <th>N°</th>
-                            <th>Prenom et Nom</th>
-                            <th>Date de naissance</th>
-                            <th>Expiration du Droit</th>
-                            <th>Observation</th>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
+            </tbody>
+
+        </table>
+
+
+            <h3 style="text-align: center; position: relative; left:10">ENFANTS A CHARGE</h3>
+            <table style="width: 100%">
+                <thead>
+                    <tr>
+                        <th>N°</th>
+                        <th>Prenom et Nom</th>
+                        <th>Date de naissance</th>
+                        <th>Expiration du Droit</th>
+                        <th>Observation</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
                             $i=1;
                         @endphp
 
@@ -141,30 +138,28 @@
                         @endforeach
 
 
+                </tbody>
+            </table>
+            <div style="text-align: right; margin-top:10px">
+                Le Chef de Service:
 
-                    </tbody>
-                </table>
-               </tr>
-            </tbody>
-
-        </table>
-
-
-            <div style="position: relative; top: 80">
-                {!! DNS2D::getBarcodeHTML($no_imma, 'QRCODE',5,5) !!}
             </div>
-
-
-        <div style="position:absolute; bottom:2">
-            <div style="width: 20%; font-size: 11px;position: relative; top:30">
-                <img src="{{ public_path('flagui.png') }}" width="100" height="40">
+            <div style="text-align: left; margin-top:10px">
+                Conakry {{$date}}
             </div>
+        <div style="position: absolute; bottom:-25">
+            <img src="{{ public_path('branding.png') }}" width="100" height="70">
+        </div>
 
-            <div style="text-align: center;font-size: 11px; position: relative; left:200">
-                <span style="text-align: center; font-weight:bold">République de Guinée</span> <br>
-                <span style="text-align: center;">Caisse Nationale de sécurité Sociale, Kouléwondy - Kaloum BP 138</span> <br>
-                <span>République de Guinée | www.cnss.gov.gn</span>
-            </div>
+
+            <div style="width: 100%; font-size: 11px; position: absolute; top:-5">
+                <img src="{{ public_path('flagui.png') }}" width="700" height="10">
+             </div>
+
+         <div style="text-align: center; position: absolute; bottom:-20; left: 150;font-size: 11px;">
+            <span style="text-align: center; font-weight:bold">République de Guinée</span> <br>
+            <span style="text-align: center;">Caisse Nationale de sécurité Sociale, Kouléwondy - Kaloum BP 138</span> <br>
+            <span>République de Guinée | www.cnss.gov.gn</span>
         </div>
 
 
@@ -173,159 +168,3 @@
 </html>
 
 
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fiche paie</title>
-</head>
-<body style="font-family: 'Poppins',sans-serif">
-
-    <table style="width: 70%;margin-bottom:10px; border:1px solid black">
-        <tbody>
-           <tr style="margin-top: 0 !important">
-
-               <td>
-                <img src="{{ public_path('LOgo-CNSS.png') }}" width="200" height="100">
-
-                </td>
-               <td>
-                  <span style="position: relative; top:20; font-size:12px;"> REPUBLIQUE DE GUINEE</span>
-
-                   <div style="width:%; margin-left:0px;position: relative; top:20">
-                     <table style="width: 100%">
-                        <tr>
-                            <td style="background-color: red; width:85%; height:2px"></td>
-                            <td style="background-color: yellow; width:75%"></td>
-                            <td style="background-color: green; width:75%"></td>
-                        </tr>
-                     </table>
-                  </div>
-                    <div style="margin-left: 20px;position: relative; top: 20 "> <span style="font-size:11px;">Travail-Justice-Solidarite</span></div><br><br>
-
-               </td>
-
-
-           </tr>
-           <tr>
-                <h3 style="text-align: center; position: relative; left:60">PENSION DE RETRAITE</h3>
-                <h1 style="text-align: center; position: relative; left:60">FICHE DE PAIE</h1>
-                <h2 style="text-align: center; position: relative; left:60"> <span style="color: red"> N° 01-506780 </span> </h2>
-                <h2 style="text-align: center; position: relative; left:60">ASSIGNATION</h2>
-
-                <ol>
-                    <li>BOKÉ</li>
-                </ol>
-
-           </tr>
-        </tbody>
-        <img src="{{ public_path('cnsslogo2.png') }}" width="300" height="300"
-        style=" opacity: 0.1; position:absolute; left: 95px; top:100px;">
-    </table>
-    <div style="position: relative; top:">
-        {!! DNS2D::getBarcodeHTML($no_imma, 'QRCODE',5,5) !!}
-    </div>
- <h5>Le Chef de Service:</h5>  <p style="text-align: right; position: relative; right:400">Conakry: {{$date}}</p>
-    <table style="width: 50%;margin-bottom:10px; position:absolute; top:0;left:400;border:1px solid black">
-        <tbody>
-           <tr style="margin-top: 0 !important">
-
-              <td>
-                <img src="{{ storage_path('app/public/pensionnaireImg/'.$photo) }}" style="width:100px; height:100px; margin-left:200px" alt="" srcset="">
-                </td>
-
-           </tr>
-           <tr>
-            <table style="margin-top: 20px">
-                <tbody>
-                    <tr>
-                        <th style="width: 30%; text-align:left; font-size: 13px">PRENOMS:</th>
-                        <td style="font-size: 13px">{{ $prenom }}</td>
-                    </tr>
-                    <tr>
-                        <th style="width: 30%; font-size: 13px; text-align:left">NOM:</th>
-                        <td style="font-size: 13px">{{ $nom }}</td>
-                    </tr>
-                    <tr>
-                        <th style="width: 30%; font-size: 13px; text-align:left">Retraité(e) le:</th>
-                        <td style=" font-weight:bold;font-size: 13px"> {{$date_end_job}}</td>
-                    </tr>
-                    <tr>
-                        <th style="width: 30%; font-size: 13px; text-align:left">DERNIER EMPLOYEUR:</th>
-                        <td style="font-size: 13px; color: red;">{{ $employer }}</td>
-                    </tr>
-                    <tr>
-                        <th style="width: 30%; text-align:left; font-size: 13px">MONTANT TRIMESTRIEL:</th>
-                        <td style="font-size: 13px">{{ $mont_trimestre }}</td>
-                    </tr>
-                    <tr>
-                        <th style="width: 30%; font-size: 13px; text-align:left">MONTANT TRIMESTRIEL REVALORISÉ:</th>
-                        <td style="font-size: 13px">{{ $mont_trimestre_revalorise }}</td>
-                    </tr>
-                    <tr>
-                        <th style="width: 30%; font-size: 13px; text-align:left">MONTANT AF:</th>
-                        <td style=" font-weight:bold;font-size: 13px">{{ $mont_AF }}</td>
-                    </tr>
-                    <tr>
-                        <th style="width: 30%; font-size: 13px; text-align:left">MONTANT TOTAL PENSION:</th>
-                        <td style="font-size: 13px; color: red;">{{ $mont_total_pension }}</td>
-                    </tr>
-                    <tr>
-                        <th style="width: 30%; text-align:left; font-size: 13px">DATE DE JOUISSANCE:</th>
-                        <td style="font-size: 13px; color: red;"></td>
-                    </tr>
-
-
-                </tbody>
-
-            </table>
-           </tr>
-           <tr>
-            <h3 style="text-align: center; position: relative; left:10">ENFANTS A CHARGE</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>N°</th>
-                        <th>Prenom et Nom</th>
-                        <th>Date de naissance</th>
-                        <th>Expiration du Droit</th>
-                        <th>Observation</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($enfants as $enf)
-
-
-                    <tr>
-                        <td>{{ $loop->index+1 }}</td>
-                        <td style="text-align: center">{{ $enf->prenom_enfant }} {{ $enf->nom_enfant }}</td>
-                        <td style="text-align: center"> {{ \Carbon\Carbon::parse($enf->date_naissance_enfant)->format('d/m/Y')}}</td>
-                        <td style="color: red; text-align: center">ici<</td>
-                        <td>ici</td>
-
-                    </tr>
-
-                    @endforeach
-
-
-                </tbody>
-            </table>
-           </tr>
-        </tbody>
-
-    </table>
-
-    <div style="width: 20%; font-size: 11px; position: absolute; bottom:-15">
-        <img src="{{ public_path('flagui.png') }}" width="100" height="40">
-     </div>
-
- <div style="text-align: center; position: absolute; bottom:-20; left: 300;font-size: 11px;">
-    <span style="text-align: center; font-weight:bold">République de Guinée</span> <br>
-    <span style="text-align: center;">Caisse Nationale de sécurité Sociale, Kouléwondy - Kaloum BP 138</span> <br>
-    <span>République de Guinée | www.cnss.gov.gn</span>
-</div>
-</body>
-</html> --}}
